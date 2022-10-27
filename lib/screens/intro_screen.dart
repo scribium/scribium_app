37d5c5044370cb03825da3be53b3e6452db8875d
iDarkQ <54110160+iDarkQ@users.noreply.github.com>
2022-10-27 10:06:20 +0100

    delete(screens): Remove custom animated routing (#13)
    * feat: Begin authentication
    
    Create user model, and auth change notifier
    
    * feat: Add Main Panel Screen
    
    * change: Finish the login screen layout
    
    * change: General correcitons
    
    Change the import name, Improve code quality, wrap some code.
    
    * feat: Add scribium overlay settings
    
    Change font in the large title.
    
    * change: Login inputs
    
    Add new animation, avoid memory leaks through controllers, add hacks.
    
    * feat: Add some concepts
    
    Add NavigationBar, Change ui style, Add appbar
    
    * feat: Add flutter staggered grid view dependency
    
    * change: Add new styling in the theme settings
    
    * feat(components): Add new custom card widget
    
    * feat(components): Add empty box widget
    
    * delete(screens): Remove custom animation routing

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -17,42 +17,18 @@ class _IntroScreenState extends State<IntroScreen> {
     super.initState();
 
     Future.delayed(const Duration(milliseconds: 1500), () {
-      Navigator.of(context).push(_createRoute());
+      Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
       body: SafeArea(
         child: Center(
           child: Image.asset("assets/images/scribium-logo.png"),
         ),
       ),
-    );
-  }
-
-  Route _createRoute() {
-    // TODO: Check if user has already an account
-    // TODO: Skip login if the user is logged
-    return PageRouteBuilder(
-      transitionDuration: const Duration(milliseconds: 1500),
-      pageBuilder: (context, animation, secondaryAnimation) =>
-          const LoginScreen(),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        const begin = 0.0;
-        const end = 1.0;
-        const curve = Curves.easeOutQuart;
-
-        var tween =
-            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-        return FadeTransition(
-          opacity: animation.drive(tween),
-          child: child,
-        );
-      },
     );
   }
 }
