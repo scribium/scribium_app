b95a91630eef740f00945b07dfb380e8a160e5d9
iDarkQ <54110160+iDarkQ@users.noreply.github.com>
2022-11-08 22:42:59 +0000

    change(screens): Adjust main panel
    Set fixed height for the appbar
    Prepare application for phone, tablet, and laptop sizes
    Add new navigation bar option "Settings"
    Remove scrollbar
    Add bouncing scroll physics

--- lib/screens/main_panel_screen.dart
+++ lib/screens/main_panel_screen.dart
@@ -7,6 +7,8 @@ import '../components/empty_box.dart';
 import '../components/main/assignments_list.dart';
 import '../components/main/main_grid_panel.dart';
 
+import '../components/card.dart' as c;
+
 class MainPanelScreen extends StatefulWidget {
   static const routeName = "/main";
   const MainPanelScreen({super.key});
@@ -36,65 +38,136 @@ class _MainPanelScreenState extends State<MainPanelScreen> {
       ScribiumSystemUiOverlayStyle.light,
     );
 
+    // TODO: Check if the app is opened on mobile or (laptop / tablet)
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       extendBodyBehindAppBar: true,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(
-          (mQD.size.height - mQD.padding.vertical) * 0.25,
-        ),
-        child: MainAppBar(
-          getScrollController: getScrollController,
-          mQD: mQD,
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.dashboard),
-            label: "Dashboard",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.assignment),
-            label: "Grades",
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        controller: _scrollController,
-        child: Column(
-          children: [
-            EmptyBox(
-              mQD: mQD,
-              height: 0.275,
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              width: double.infinity,
-              height: (mQD.size.height - mQD.padding.vertical) * 0.0325,
-              child: Text(
-                "Assignments",
-                style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w100,
-                      fontFamily: "Archivo",
-                    ),
-                textAlign: TextAlign.left,
+      appBar: mQD.size.width <= Constants.largeBreakPoint
+          ? PreferredSize(
+              preferredSize: const Size.fromHeight(
+                300,
               ),
-            ),
-            SizedBox(
-              height: (mQD.size.height - mQD.padding.vertical) * 0.15,
-              child: const AssignmentsList(),
-            ),
-            SizedBox(
-              height: (mQD.size.height - mQD.padding.vertical) * 1,
-              child: MainGridPanel(
+              child: MainAppBar(
+                getScrollController: getScrollController,
                 mQD: mQD,
               ),
-            ),
-          ],
-        ),
-      ),
+            )
+          : null,
+      bottomNavigationBar: mQD.size.width <= Constants.largeBreakPoint
+          ? BottomNavigationBar(
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.dashboard),
+                  label: "Dashboard",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.assignment),
+                  label: "Grades",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  label: "Settings",
+                ),
+              ],
+            )
+          : null,
+      body:
+
+          // PHONE
+
+          mQD.size.width <= Constants.largeBreakPoint
+              ? ScrollConfiguration(
+                  behavior: ScrollConfiguration.of(context)
+                      .copyWith(scrollbars: false),
+                  child: SingleChildScrollView(
+                      controller: _scrollController,
+                      physics: const BouncingScrollPhysics(),
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: kToolbarHeight + 120,
+                          ),
+                          // EmptyBox(
+                          //   mQD: mQD,
+                          //   height: 0.275,
+                          // ),
+                          Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 20),
+                            width: double.infinity,
+                            child: Text(
+                              "Assignments",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w100,
+                                    fontFamily: "Archivo",
+                                  ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 100,
+                            child: AssignmentsList(),
+                          ),
+                          SizedBox(
+                            height:
+                                (mQD.size.height - mQD.padding.vertical) * 1,
+                            child: MainGridPanel(
+                              mQD: mQD,
+                            ),
+                          ),
+                        ],
+                      )),
+                )
+
+              // TABLET / COMPUTER
+
+              : SizedBox(
+                  width: (mQD.size.width - mQD.padding.horizontal) * 1,
+                  child: Row(
+                    children: [
+                      c.Card(
+                        child: NavigationRail(
+                          selectedIndex: 0,
+                          groupAlignment: -1.0,
+                          labelType: NavigationRailLabelType.selected,
+                          leading: Container(
+                            margin: const EdgeInsets.only(bottom: 20),
+                            child: Column(
+                              children: const [
+                                Icon(
+                                  Icons.account_box_rounded,
+                                  size: 40,
+                                ),
+                                Text("Łukasz"),
+                              ],
+                            ),
+                          ),
+                          destinations: const <NavigationRailDestination>[
+                            NavigationRailDestination(
+                              icon: Icon(Icons.dashboard),
+                              selectedIcon: Icon(Icons.dashboard),
+                              label: Text('DashBoard'),
+                            ),
+                            NavigationRailDestination(
+                              icon: Icon(Icons.assignment),
+                              selectedIcon: Icon(Icons.assignment),
+                              label: Text('Grades'),
+                            ),
+                            NavigationRailDestination(
+                              icon: Icon(Icons.settings),
+                              selectedIcon: Icon(Icons.settings),
+                              label: Text('Settings'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
     );
   }
 }
