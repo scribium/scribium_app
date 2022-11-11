7c131602e4adfbed6661a31e97e483735a02f954
iDarkQ <54110160+iDarkQ@users.noreply.github.com>
2022-11-11 09:13:00 +0000

    change(components): Changes in main grid panel
    Remove random generated grids
    Insert created grids
    Put in the comment margins for grids
    Add bottom margin

--- lib/components/main/main_grid_panel.dart
+++ lib/components/main/main_grid_panel.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:scribium_app/components/main/calendar_grid.dart';
-import 'package:scribium_app/extensions/number_extension.dart';
-import 'package:scribium_app/utilities/constants.dart';
+import 'package:scribium_app/components/main/frequency_grid.dart';
+import 'package:scribium_app/components/main/timetable_grid.dart';
+import 'package:scribium_app/components/main/warnings_grid.dart';
 
 import 'grades_grid.dart';
 
@@ -12,60 +13,28 @@ class MainGridPanel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
       width: double.infinity,
       child: StaggeredGrid.count(
         crossAxisCount: 2,
-        children: [
+
+        /*
+
+        margin: (i / 2).isInteger
+    ? const EdgeInsets.only(
+        right: 15, left: 5, top: 10) // RIGHT
+    : const EdgeInsets.only(
+        right: 5, left: 15, top: 10), //LEFT
+
+        */
+        children: const [
           ...[
-            const GradesGrid(),
-            const CalendarGrid(),
-            for (int i = 0; i < 5; i++)
-              AspectRatio(
-                aspectRatio: 1 / 1,
-                child: Container(
-                  //TODO: Default 15
-                  margin: (i / 2).isInteger
-                      ? const EdgeInsets.only(right: 15, left: 5, top: 10) // RIGHT
-                      : const EdgeInsets.only(right: 5, left: 15, top: 10), //LEFT
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Theme.of(context).shadowColor,
-                        blurRadius: 20,
-                        spreadRadius: 5,
-                      ),
-                    ],
-                  ),
-                  child: Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                      onTap: () {},
-                      borderRadius: BorderRadius.circular(15),
-                      child: FittedBox(
-                        fit: BoxFit.scaleDown,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Icon(Icons.grade),
-                            Text(
-                              "SOON",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .titleSmall!
-                                  .copyWith(
-                                    color: ScribiumColors.darkPurple,
-                                  ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+            GradesGrid(),
+            CalendarGrid(),
+            WarningsGrid(),
+            TimeTableGrid(),
+            FrequencyGrid(),
           ]
         ],
       ),
